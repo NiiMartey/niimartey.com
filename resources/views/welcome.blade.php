cc597a28de06533baab0aef4d8992748093edcf7
Martey Newman-Adjiri <niimartey.gh@gmail.com>
2017-08-26 16:44:58 +0000

    setting up .env and databse

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Nii Martey
                 </div>
 
                 <div class="links">
